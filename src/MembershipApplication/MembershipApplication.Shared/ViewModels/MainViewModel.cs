9577c65fd1a871f841314384bd65464566eb7583
Markus Malz <markus.malz@outlook.com>
2022-04-24 15:33:00 +0200

    Add some first fields

--- src/MembershipApplication/MembershipApplication.Shared/ViewModels/MainViewModel.cs
+++ src/MembershipApplication/MembershipApplication.Shared/ViewModels/MainViewModel.cs
@@ -4,11 +4,37 @@ namespace MembershipApplication.ViewModels
 {
     public class MainViewModel : ObservableRecipient
     {
+        private bool _isChildApplication;
+        public bool IsChildApplication
+        {
+            get => _isChildApplication;
+            set
+            {
+                SetProperty(ref _isChildApplication, value);
+                IsAdultApplication = !value;
+            }
+        }
+
+        private bool _isAdultApplication = true;
+        public bool IsAdultApplication
+        {
+            get => _isAdultApplication;
+            set => SetProperty(ref _isAdultApplication, value);
+        }
+
         private int _id;
         public int Id
         {
             get => _id;
             set => SetProperty(ref _id, value);
+        }
+
+        private string _birthday;
+
+        public string Birthday
+        {
+            get => _birthday;
+            set => SetProperty(ref _birthday, value);
         }
     }
 }
