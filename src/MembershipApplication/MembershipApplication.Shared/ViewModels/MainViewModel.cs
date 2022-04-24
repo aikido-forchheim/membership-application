af1ea1ade7199c5a94919dcfae04b000205bda73
Markus Malz <markus.malz@outlook.com>
2022-04-24 09:17:57 +0200

    Add first sample with TwoWay binding

--- src/MembershipApplication/MembershipApplication.Shared/ViewModels/MainViewModel.cs
+++ src/MembershipApplication/MembershipApplication.Shared/ViewModels/MainViewModel.cs
@@ -4,6 +4,23 @@ public class MainViewModel : MvxViewModel
 {
     public string Message { get => MessageService.GetMessage(); }
 
+    private bool _rememberMe;
+    public bool RememberMe
+    {
+        get => _rememberMe;
+        set => SetProperty(ref _rememberMe, value);
+    }
+
+    private bool _rememberMe2;
+    public bool RememberMe2
+    {
+        get => _rememberMe2;
+        set {
+            SetProperty(ref _rememberMe2, value);
+            RememberMe = value;
+        } 
+    }
+
     protected IMessageService MessageService { get; }
      
     public MainViewModel(IMessageService messageService)
