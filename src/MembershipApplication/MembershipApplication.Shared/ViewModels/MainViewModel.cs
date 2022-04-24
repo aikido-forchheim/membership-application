c27f92806196204a4ec103ef9c88b42f0e5759c5
Markus Malz <markus.malz@outlook.com>
2022-04-24 13:50:48 +0200

    Add Id

--- src/MembershipApplication/MembershipApplication.Shared/ViewModels/MainViewModel.cs
+++ src/MembershipApplication/MembershipApplication.Shared/ViewModels/MainViewModel.cs
@@ -1,7 +1,14 @@
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
 namespace MembershipApplication.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : ObservableRecipient
     {
-        
+        private int _id;
+        public int Id
+        {
+            get => _id;
+            set => SetProperty(ref _id, value);
+        }
     }
 }
